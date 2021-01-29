978f3919c10559941ff66e7e5b7494a426cbc898
tonifarley <toni@exergipredictive.com>
2021-01-29 10:26:19 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='tinytest',
+    version='1.0',
+    author_email='toni.farley@gmail.com',
+    install_requires=[
+        'requests>=2.25'
+    ]
+)
